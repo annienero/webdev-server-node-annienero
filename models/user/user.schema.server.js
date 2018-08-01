3a3942a46211b2574991a6c0cdf8b9aa5c530b98
Annie <annienero93@gmail.com>
2018-08-01 10:27:05 -0400

    create user schema

---
+++ models/user/user.schema.server.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+var userSchema = mongoose.Schema({
+    username: String,
+    password: String,
+    firstName: String,
+    lastName: String,
+    role: String
+}, {collection: 'user'})
+
+module.exports = userSchema;
