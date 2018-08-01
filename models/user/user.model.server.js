33534d3c2b3f6f49bc3588a325c5b8ebad2308b0
Annie <annienero93@gmail.com>
2018-08-01 10:26:59 -0400

    create user model

---
+++ models/user/user.model.server.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+var userSchema = require('./user.schema.server')
+
+var userModel = mongoose.model('UserModel', userSchema)
+
+function findAllUsers() {
+    return userModel.find()
+}
+
+function findUserById(userId) {
+    return userModel.findById(userId)
+}
+
+function findUserByUsername(username) {
+    return userModel.findOne({username: username})
+}
+
+function findUserByCredentials(username, password) {
+    return userModel.findOne({username: username, password: password})
+}
+
+function createUser(user) {
+    return userModel.create(user)
+}
+
+function deleteUser(userId) {
+    return userModel.remove({_id: userId})
+}
+
+function updateUser(userId, newUser) {
+    return userModel.update({_id: userId},
+        {$set: newUser})
+}
+
+module.exports = {
+    findAllUsers: findAllUsers,
+    findUserById: findUserById,
+    findUserByUsername: findUserByUsername,
+    findUserByCredentials: findUserByCredentials,
+    createUser: createUser,
+    deleteUser: deleteUser,
+    updateUser: updateUser
+};
