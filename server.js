a3d6814692df714fce08e906277eb5564b3df068
Annie <annienero93@gmail.com>
2018-08-06 15:56:25 -0400

    testing

--- server.js
+++ server.js
@@ -16,15 +16,14 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin",
-        "http://localhost:4200") //TODO local
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept")
-    res.header("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS")
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200") //TODO local
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
     res.header("Access-Control-Allow-Credentials", "true")
     next()
 })
+
+app.get('/hello', (req, res) => {res.send('hello from heroku')});
 
 var userService = require('./services/user.service.server')
 userService(app)
