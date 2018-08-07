c3af24e5dd7de4a90d5b3f6d40d483a918ff9915
Annie <annienero93@gmail.com>
2018-08-07 15:18:47 -0400

    add choices to question

--- models/question/question.schema.server.js
+++ models/question/question.schema.server.js
@@ -4,6 +4,11 @@ var questionSchema = mongoose.Schema({
     title: String,
     points: Number,
     description: String,
+    choices: [{
+        text: String,
+        value: String,
+        correct: Boolean
+    }],
     questionType: {
         type: String,
         enum: ['ESSAY', 'FILL_BLANKS', 'TRUE_FALSE', 'CHOICE']
