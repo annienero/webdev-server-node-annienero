28bf65c76c2aef9589bf667c5af294f18fdde4d1
Annie <annienero93@gmail.com>
2018-08-08 17:41:11 -0400

    create submission schema

---
+++ models/submission/submission.schema.server.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+var submissionSchema = mongoose.Schema({
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UserModel'
+    },
+    quiz: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'QuizModel'
+    },
+    answers: [{
+        question: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'QuestionModel'
+        },
+        fillBlanksAnswers: {variable: String, value: String},
+        multipleChoiceAnswer: String,
+        essayAnswer: String,
+        trueFalseAnswer: Boolean
+    }]
+}, {collection: 'submission'})
+
+module.exports = submissionSchema;
