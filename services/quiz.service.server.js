19ccbbf96eef4f7e714c007287a17e91c78fcb24
Annie <annienero93@gmail.com>
2018-08-08 17:41:40 -0400

    add submit quiz to quiz service

--- services/quiz.service.server.js
+++ services/quiz.service.server.js
@@ -1,9 +1,11 @@
 module.exports = function (app) {
     app.get('/api/quiz', findAllQuizzes);
     app.get('/api/quiz/:quizId', findQuizById);
+    app.post('/api/quiz/:quizId/submission', submitQuiz);
 }
 
 var quizModel = require('../models/quiz/quiz.model.server');
+var submissionModel = require('../models/submission/submission.model.server');
 
 function findAllQuizzes(req, res) {
     quizModel.findAllQuizzes()
@@ -14,3 +16,10 @@ function findQuizById(req, res) {
     quizModel.findQuizById(req.params['quizId'])
         .then(function(quiz) { res.json(quiz) })
 }
+
+function submitQuiz(req, res) {
+    //TODO much parse
+    submissionModel.createSubmission(req.body)
+    .then(function(submission) { res.json(submission) })
+}
+
